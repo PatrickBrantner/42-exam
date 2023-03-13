79351ec859dcb2414e0eb6080fc9747e4506b693
PatrickBrantner <118632408+PatrickBrantner@users.noreply.github.com>
2023-03-13 18:33:09 +0100

    com3

--- exam02/03/epur_str.c
+++ exam02/03/epur_str.c
@@ -2,27 +2,27 @@
 
 int     main(int argc, char **argv)
 {
+    int i = 0;
+    int j = 0;
+    
     if (argc == 2)
     {
-        int i = 0;
-        while (argv[1][i])
+        while (argv[1][j] != '\0')
+            j++;
+        while (argv[1][j - 1] == ' ' || argv[1][j - 1] == '\t')
+            j--;
+        while (i < j)
         {
             while (argv[1][i] == ' ' || argv[1][i] == '\t')
                 i++;
-            while (argv[1][i] != ' ' && argv[1][i] != '\t' && argv[1][i])
+            while (argv[1][i] != ' ' && argv[1][i] != '\t' && argv[1][i] != '\0')
             {
-                    write(1, &argv[1][i],1);
-                    i++;
+                write(1,&argv[1][i],1);
+                i++;
             }
-            if (argv[1][i] && argv[1][i + 1])
+            if (i != j)
                 write(1," ",1);
         }
     }
     write(1,"\n",1);
 }
-                    
-
-
-
-                    
-
