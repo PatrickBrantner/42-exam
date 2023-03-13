2ce96823022b2f85f013619e3f5168d5c1d5e275
PatrickBrantner <118632408+PatrickBrantner@users.noreply.github.com>
2023-03-13 20:19:54 +0100

    fixed ft_atoi_base

--- exam02/03/ft_atoi_base.c
+++ exam02/03/ft_atoi_base.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-int     ft_convert(char c, int base)
+int     ft_putnbr(const char c, int str_base)
 {
+    int numb;
     int i = 0;
     char table1[] = "0123456789abcdef";
     char table2[] = "0123456789ABCDEF";
 
-
-    while( i <= base)
+    while (i <= str_base)
     {
         if (c == table1[i] || c == table2[i])
             return i;
-        i++;   
-    }
+        i++;
+    }   
     return (-1);
 }
 
-int	    ft_atoi_base(const char *str, int str_base)
+
+int	ft_atoi_base(const char *str, int str_base)
 {
-    int nbr = 0;
-    int i = 0;
     int sign = 1;
-    
+    int i = 0;
+    int numb = 0;
+
     while (str[i] == ' ' || str[i] == '\t')
         i++;
     if (str[i] == '-' || str[i] == '+')
     {
-        if(str[0] == '-')
-            sign *= -1;
+        if (str[i] == '-')
+            sign = -1;
         i++;
     }
-    while (str[i])
+    while(str[i] != '\0' && ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'f') || (str[i] >= 'A' && str[i] <= 'F')))
     {
-        if (ft_convert(str[i], str_base) == -1)
-            return(0);
-        nbr = (nbr * str_base) + ft_convert(str[i], str_base);
+        if (ft_putnbr(str[i], str_base) == -1)
+            return 0;
+        numb = (numb * str_base) + ft_putnbr(str[i], str_base);
         i++;
     }
-    return (sign * nbr);
+    return (numb * sign);
 }
 
-int     main()
+int     main(int argc, char **argv)
 {
-    printf("%d\n", ft_atoi_base("+111", 1));
-
+    printf("%d\n", ft_atoi_base(argv[1], atoi(argv[2])));
 }
