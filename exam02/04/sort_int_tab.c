6d4ce524a509c5ff49acd7b29e047d6254068f66
PatrickBrantner <118632408+PatrickBrantner@users.noreply.github.com>
2023-04-27 15:34:01 +0200

    Update sort_int_tab.c

--- exam02/04/sort_int_tab.c
+++ exam02/04/sort_int_tab.c
@@ -4,6 +4,9 @@ void sort_int_tab(int *tab, unsigned int size)
 {
     unsigned int i = 0;
     int temp;
+    
+    if(!tab || !size)
+        return ;    
     while (i < size - 1)
     {
         if (tab[i] > tab[i + 1])
