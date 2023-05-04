75f633a9b0a8ac415601297fdfc7571403f862ab
PatrickBrantner <118632408+PatrickBrantner@users.noreply.github.com>
2023-05-04 10:04:55 +0200

    Update swap_bits.c

--- exam02/02/swap_bits.c
+++ exam02/02/swap_bits.c
@@ -1,4 +1,28 @@
-/* #include <unistd.h>
+/*
+Assignment name  : swap_bits
+Expected files   : swap_bits.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that takes a byte, swaps its halves (like the example) and
+returns the result.
+
+Your function must be declared as follows:
+
+unsigned char	swap_bits(unsigned char octet);
+
+Example:
+
+  1 byte
+_____________
+ 0100 | 0001
+     \ /
+     / \
+ 0001 | 0100
+ */
+
+/* 
+#include <unistd.h>
 
 void	print_bits(unsigned char octet)
 {
@@ -10,7 +34,8 @@ void	print_bits(unsigned char octet)
         write(1,&bit,1);
         i--;
     }
-} */
+} 
+*/
 
 unsigned char	swap_bits(unsigned char octet)
 {
